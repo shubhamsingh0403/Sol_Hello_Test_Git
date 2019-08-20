8f297122e1a411c34d650b8c4bbc7c5247edc86e
SHUBHAM SINGH <shubham20181920@outlook.com>
2019-08-20 17:26:29 +0530

    code commit

---
+++ Sol_Hello_Git/Sol_Hello_Git/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sol_Hello_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Git");
+        }
+    }
+}
